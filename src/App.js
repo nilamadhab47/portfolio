2737c2a9db759b8b0595e715b9b537cbd6c21a3c
Nilamadhab Senapati <79541831+nilamadhab47@users.noreply.github.com>
2022-11-06 02:12:13 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import GlobalStyles from './GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+import { DarkTheme, lightTheme } from './component/Theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <GlobalStyles/>
+    <ThemeProvider theme={DarkTheme}>
+    PortFolio
+    </ThemeProvider>
+
+    </>
   );
 }
 
